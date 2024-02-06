95c985d25340ba922fca531ecfc882b1d1886afe
Armaan H <97925527+ArmaanHajar@users.noreply.github.com>
2024-02-06 10:37:03 -0800

    recursive functions added

--- ImarisPipeline.py
+++ ImarisPipeline.py
@@ -1,7 +1,7 @@
 """
 Imaris Image Processing Pipeline
 Author: Armaan Hajar and Chandler Asnes
-Date: 1/30/23
+Date: 2/5/23
 """
 
 import pyautogui as auto
@@ -9,58 +9,6 @@ import time as tm
 import os
 import shutil
 from xlwt import Workbook
-
-"""
-1. Convert file @@@@
-2. Set statistics you want @@@@
-3. Add new surface
-4. Go to page 1/4 on surfaces
-5. Uncheck “Classify Surfaces”
-6. Uncheck “Object-Object Statistics
-7. Go to page 2/4
-8. Select “Background Subtraction (Local Contrast)”
-9. Set “Diameter of largest Sphere which fits into the Object” @@@@
-10. Go to page 3/4
-11. Adjust “Threshold (Background Subtraction)“ @@@@
-12. Go to page 4/4
-13. Adjust Filter @@@@
-14. Press green arrow
-15. Remove unwanted artifacts @@@
-16. Select created surface
-17. Press “Edit” pencil
-18. Press “Mask All”
-19. Check “Set voxel intensity inside surface to”
-20. Set to 10000
-21. Press enter
-22. Enter “Edit” menu
-23. Press “Show Display Adjustment”
-24. Uncheck “W1 - TRITC” (or first option)
-25. Press “Add new Filaments” (little green leaf)
-26. Uncheck “Object-Object Statistics”
-27. Go to page 2/10
-28. Change “Select Source Channel” to “Channel 2 - Masked”
-29. Uncheck “Turn on/off slicer rendering for selected object” (little yellow box)
-30. Set “Estimated Largest Diameter" @@@@
-31. Go to page 3/10
-32. Set “Starting Points Threshold" @@@@
-33. Set Starting Points @@@@
-34. Uncheck “Calculate Soma Model”
-35. Go to page 4/9
-36. Set “Thinnest Diameter” to 10
-37. Go to page 5/9
-38. Uncheck “Classify Seed Points”
-39. Uncheck “Classify Segments”
-40. Adjust “Seed Points Threshold” @@@@
-41. Go to page 6/6
-42. Press green arrow if you are satisfied with results @@@@
-43. Press Statistics (mini graph)
-44. Press "Export all Statistics to file" (several floppy disks)
-45. Open "Exported Excel Data"
-46. Open folder user inputted at the start of the program
-47. Press Save
-48. Long pause until user is done
-49. Alt F4 to close excel
-"""
 
 source_folder = r'D:\Chandler\Raw Imaris Files'
 destination_folder = r'D:\Chandler\Processed Imaris Files'
@@ -82,66 +30,81 @@ def launch_app():
     auto.press("enter")
 
 def save_statistics(excel_file_path):
-    find_and_click("statistics.png") # Press "Statistics" (mini graph)
-    tm.sleep(4)
-    find_and_click("exportstatistics.png") # Press "Export all Statistics to File" (several floppy disks)
+    find("statistics.png") # Press "Statistics" (mini graph)
+    try_until_found("exportstatistics.png", .9) # Press "Export all Statistics to File" (several floppy disks)
     tm.sleep(0.5)
-    find_and_click("thispc.png") # Press "This PC"
+    find("thispc.png") # Press "This PC"
     tm.sleep(0.5)
-    find_and_click("ddrive.png") # Open D Drive
+    find("ddrive.png") # Open D Drive
     auto.press('enter')
     tm.sleep(0.5)
-    find_and_click("searchdrive.png")
+    find("searchdrive.png")
     auto.typewrite(excel_file_path)
     tm.sleep(1)
-    find_and_click("folder.png")
+    find("folder.png")
     tm.sleep(0.5)
     auto.press('enter')
     tm.sleep(0.5)
-    find_and_click("save.png")
+    find("save.png")
     tm.sleep(2)
     auto.hotkey('alt', 'f4')
 
 def open_next_file():
-    find_and_click("openfolder.png")
+    find("openfolder.png")
     tm.sleep(0.5)
     auto.press('enter')
-    tm.sleep(11)
+    try_until_found("open_iif.png", .9)
     auto.press('tab', 9)
     auto.press('right', 2)
     auto.press('enter')
 
-def find_and_click(button_name):
+def try_until_found(looking: str, ci: float, count = 0):
+    if count > 500: # Base case: stop if more than 500 attempts to find have been made
+        print("Unable to Find")
+        return
+
     path = r'D:\Chandler\Imairs Pipeline\Button Images'
-    os.chdir(path)
+    full_path = os.path.join(path, looking)
 
-    if button_name == 'selectsourcechannel.png' or button_name == 'channel2.png':
-        location = auto.locateOnScreen(button_name, confidence=.89)
-    elif button_name == 'statistics.png' or button_name == "w1tritc.png":
-        location = auto.locateOnScreen(button_name, confidence=.9)
-    elif button_name == 'nextbutton.png':
-        location = auto.locateOnScreen(button_name, confidence=.995)
-    elif button_name == "x_out":
-        try:
-            location = auto.locateOnScreen("x_out.png", confidence=.966)
-        except:
-            location = auto.locateOnScreen("x_out_gray.png", confidence=.966)
-    else:
-        location = auto.locateOnScreen(button_name, confidence=.966)
+    try:
+        location = auto.locateOnScreen(full_path, confidence=ci)
+        if location:
+            if looking == "exportstatistics.png":
+                find("exportstatistics.png")
+            else:
+                return
+        else:
+            raise Exception("Not found")
+    except Exception:
+        try_until_found(looking, ci, count + 1)
 
-    if button_name == 'thinnestdiameter.png':
-        # Triple Click "Thinnest Diameter"
-        x_ = location.left - 20
-        y_ = location.top + 10
-        auto.tripleClick(x_, y_)
-    else:
-        x_ = location.left + 10
-        y_ = location.top + 10
-        auto.click(x_, y_)
-        if button_name == 'voxelinside.png':
-            # Selects "Set voxel intensity inside surface to" Text Box
-            tm.sleep(.5)
-            auto.tripleClick(location.left + location.width - 30, y_)
+def find(button_name: str, ci = 1.0):
+    '''Recursive function to find a button on the screen at the highest confidence interval'''
+    if ci < 0.7:  # Base case: stop if confidence interval is too low
+        print("Button not found")
+        return
+
+    path = r'D:\Chandler\Imairs Pipeline\Button Images'
+    full_path = os.path.join(path, button_name)
+
+    try:
+        location = auto.locateOnScreen(full_path, confidence=ci)
+        if location:
+            if button_name == 'thinnestdiameter.png': # Triple Click "Thinnest Diameter"
+                x_ = location.left - 20
+                y_ = location.top + 10
+                auto.tripleClick(x_, y_)
+            else:
+                x_ = location.left + 10
+                y_ = location.top + 10
+                auto.click(x_, y_)
+                if button_name == 'voxelinside.png': # Selects "Set voxel intensity inside surface to" Text Box
+                    tm.sleep(.5)
+                    auto.tripleClick(location.left + location.width - 30, y_)
+        else:
+            raise Exception("Button not found")
+    except Exception:
+        find(button_name, ci - 0.01)
 
 def open_vscode():
     path = r'D:\Chandler\Imairs Pipeline\Button Images'
@@ -149,61 +112,69 @@ def open_vscode():
 
     try:
         location = auto.locateOnScreen("vscode1.png", confidence=.98)
-    except :
+    except:
         location = auto.locateOnScreen("vscode2.png", confidence=.98)
 
     center = auto.center(location)
     auto.click(center)
 
 def processing(excel_file_path):
+    path = r'D:\Chandler\Imairs Pipeline\Button Images'
+    os.chdir(path)
+
     satisfied = False
+
     tm.sleep(2)
     try:
-        find_and_click("x_out")
-    except:
+        location = auto.locateOnScreen("w1tritc.png", confidence=.9)
+        if location:
+            auto.hotkey('ctrl', 'd')
+        else:
+            raise Exception
+    except Exception:
         pass
-    find_and_click("addnewsurfaces.png")  # Presses "Add new surfaces"
-    tm.sleep(1)
-    find_and_click("classifysurfaces.png")  # Presses "Classify Surfaces"
+    find("addnewsurfaces.png")  # Presses "Add new surfaces"
     tm.sleep(0.5)
-    find_and_click("objectobjectstats.png")  # Presses "Object-Object Statistics"
+    find("classifysurfaces.png")  # Presses "Classify Surfaces"
     tm.sleep(0.5)
-    find_and_click("nextbutton.png")  # Presses Blue next page button
+    find("objectobjectstats.png")  # Presses "Object-Object Statistics"
     tm.sleep(0.5)
-    find_and_click("backgroundsubtraction.png")  # Selects "Background Subtraction (Local Contrast)" Text Box
+    find("nextbutton.png")  # Presses Blue next page button
+    tm.sleep(0.5)
+    find("backgroundsubtraction.png")  # Selects "Background Subtraction (Local Contrast)" Text Box
     tm.sleep(0.5)
     open_vscode()
     print("----------------------------------------------------------------")
     print("Select the Diameter of Largest Sphere Which Fits into the Object")
     diam_of_lar_sphere.append(float(input("What Did You Input?: ")))
     tm.sleep(2)
-    find_and_click("nextbutton.png")  # Presses Blue next page button
+    find("nextbutton.png")  # Presses Blue next page button
     tm.sleep(2)
     open_vscode()
     print("----------------------------------------------------------------")
     print("Select the Threshold (Background Subtraction)")
     background_threshold.append(float(input("What Did You Input?: ")))
     tm.sleep(2)
-    find_and_click("nextbutton.png")  # Presses Blue next page button    
+    find("nextbutton.png")  # Presses Blue next page button    
     tm.sleep(0.5)
     open_vscode()
     print("----------------------------------------------------------------")
     print("Adjust the Filter")
-    adj_filter.append((float(input("What Did You Input For the Minimum?: ")),
-                       float(input("What Did You Input For the Maximum? (if no change, input 0): "))))
+    adj_filter.append(str((float(input("What Did You Input For the Minimum?: ")),
+                           float(input("What Did You Input For the Maximum? (if no change, input 0): ")))))
     tm.sleep(2)
-    find_and_click("greennextbutton.png")  # Presses Green next page button
+    find("greennextbutton.png")  # Presses Green next page button
     tm.sleep(2)
-    find_and_click("editpencil.png")  # Presses edit pencil
+    find("editpencil.png")  # Presses edit pencil
     tm.sleep(1)
     open_vscode()
     print("----------------------------------------------------------------")
     print("Remove the Unwanted Artifacts")
     input("Hit Enter Once You've Remove the Unwanted Artifacts ")
     tm.sleep(0.5)
-    find_and_click("maskall.png")  # Presses "Mask All"
+    find("maskall.png")  # Presses "Mask All"
     tm.sleep(0.5)
-    find_and_click("voxelinside.png")  # Presses "Set voxel intensity inside surface to"
+    find("voxelinside.png")  # Presses "Set voxel intensity inside surface to"
     tm.sleep(0.5)
     auto.typewrite("10000")
     tm.sleep(0.5)
@@ -211,53 +182,49 @@ def processing(excel_file_path):
     tm.sleep(0.5)
     auto.press("enter")
     tm.sleep(3.5)
-    find_and_click("editmenu.png") # Enter Edit Menu
-    tm.sleep(1)
-    auto.press('tab', 5)
-    tm.sleep(1)
-    auto.press('enter') # Press "Show Display Adjustment"
-    tm.sleep(1)
-    find_and_click("w1tritc.png") # Uncheck "W1 - TRITC"
-    tm.sleep(1)
-    find_and_click("x_out") # Closes "Display Adjustment"
+    auto.hotkey("ctrl", "d") # Open "Show Display Adjustment"
     tm.sleep(0.5)
-    find_and_click("addnewfilaments.png") # Press "Add new Filaments" (little green leaf)
+    find("w1tritc.png") # Uncheck "W1 - TRITC"
+    tm.sleep(1)
+    auto.hotkey("ctrl", "d") # Close "Show Display Adjustment"
+    tm.sleep(0.5)
+    find("addnewfilaments.png") # Press "Add new Filaments" (little green leaf)
     tm.sleep(1.5)
-    find_and_click("objectobjectstats.png") # Uncheck "Object-Object Statistics"
+    find("objectobjectstats.png") # Uncheck "Object-Object Statistics"
     tm.sleep(0.5)
-    find_and_click("nextbutton.png")  # Presses Blue next page button    
+    find("nextbutton.png")  # Presses Blue next page button    
     tm.sleep(0.5)
-    find_and_click("selectsourcechannel.png") # Select "Select Source Channel" Dropdown
+    find("selectsourcechannel.png") # Select "Select Source Channel" Dropdown
     tm.sleep(0.5)
-    find_and_click("channel2.png") # Change Source Channel to "Channel 2 - Masked"
+    find("channel2.png") # Change Source Channel to "Channel 2 - Masked"
     tm.sleep(0.5)
-    find_and_click("slicerrendering.png") # Uncheck "Turn on/off slicer rendering for selected object" (yellow box)
+    find("slicerrendering.png") # Uncheck "Turn on/off slicer rendering for selected object" (yellow box)
     tm.sleep(0.5)
     open_vscode()
     print("----------------------------------------------------------------")
     print("Input The Estimated Largest Diameter")
     est_larg_diam.append(float(input("What Did You Input For the Estimated Largest Diameter?: ")))
     tm.sleep(2)
-    find_and_click("nextbutton.png")  # Presses Blue next page button    
+    find("nextbutton.png")  # Presses Blue next page button    
     tm.sleep(2)
     open_vscode()
     print("----------------------------------------------------------------")
     print("Set the Starting Points Threshold")
-    start_point_thres.append((float(input("What Did You Input For the Minimum?: ")),
-                              float(input("What Did You Input For the Maximum?: "))))
+    start_point_thres.append(str((float(input("What Did You Input For the Minimum?: ")),
+                                  float(input("What Did You Input For the Maximum?: ")))))
     tm.sleep(2)
-    find_and_click("calculatesomamodel.png") # Uncheck Calculate Soma Model
+    find("calculatesomamodel.png") # Uncheck Calculate Soma Model
     tm.sleep(0.5)
-    find_and_click("nextbutton.png")  # Presses Blue next page button    
+    find("nextbutton.png")  # Presses Blue next page button    
     tm.sleep(0.5)
-    find_and_click("thinnestdiameter.png") # Triple Click "Thinnest Diameter"
+    find("thinnestdiameter.png") # Triple Click "Thinnest Diameter"
     auto.typewrite("10")
     tm.sleep(0.5)
-    find_and_click("nextbutton.png")  # Presses Blue next page button    
+    find("nextbutton.png")  # Presses Blue next page button    
     tm.sleep(0.5)
-    find_and_click("classifyseedpoints.png") # Uncheck "Classify Seed Points"
+    find("classifyseedpoints.png") # Uncheck "Classify Seed Points"
     tm.sleep(0.5)
-    find_and_click("classifysegments.png") # Uncheck "Classify Segments"
+    find("classifysegments.png") # Uncheck "Classify Segments"
     tm.sleep(0.5)
     print("----------------------------------------------------------------")
     print("Adjust the Seed Points Threshold")
@@ -265,17 +232,18 @@ def processing(excel_file_path):
         open_vscode()
         temp_spt = float(input("What Did You Input For the Seed Points Threshold?: "))
         tm.sleep(0.5)
-        find_and_click("nextbutton.png")  # Presses Blue next page button    
+        find("nextbutton.png")  # Presses Blue next page button    
         tm.sleep(4)
         open_vscode()
         print("----------------------------------------------------------------")
         if input("Are You Satitsfied with the Results? (y/n): ") == 'n':
-            find_and_click("backbutton.png") # Presses back button
+            find("backbutton.png") # Presses back button
             print("Readjust the Seed Points Threshold")
         else:
             satisfied = True
+            # auto.hotkey("ctrl", s)
             seed_points_threshold.append(temp_spt)
-            find_and_click("greennextbutton.png")  # Presses Green next page button
+            find("greennextbutton.png")  # Presses Green next page button
     tm.sleep(0.5)
     save_statistics(excel_file_path)
 
@@ -284,19 +252,24 @@ def done_with_file(current_file):
     new_dest = destination_folder + f"\{current_file}"
     shutil.move(old_dest, new_dest)
 
-def save_to_excel():
+def save_to_excel(excel_file_path):
+    path = f"D:\Chandler\Exported Excel Data\{excel_file_path}"
+    os.chdir(path)
+
     wb = Workbook()
     user_input_sheet = wb.add_sheet('Data')
     names = ['File Name', 'Diameter of Largest Sphere', 'Threshold (Background Subtraction)', 'Adjust Filter',
             'Estimated Largest Diameter', 'Starting Points Threshold', 'Seed Points Threshold']
-    i_hate_myself = (names, file_names, diam_of_lar_sphere, background_threshold, adj_filter,
-                     est_larg_diam, start_point_thres, seed_points_threshold)
+    i_hate_myself = [file_names, diam_of_lar_sphere, background_threshold, adj_filter,
+                     est_larg_diam, start_point_thres, seed_points_threshold]
+    
+    for i in range(len(names)):
+        user_input_sheet.write(0, i, names[i])
+    for i in range(len(i_hate_myself)):
+        for j in range(len(file_names)):
+            user_input_sheet.write(j+1, i, i_hate_myself[i][j])
 
-    for i, li in enumerate(i_hate_myself):
-        for j, item in enumerate(li):
-            user_input_sheet.write(i, j, item)
-
-    wb.save('User Inputs.xls')
+    wb.save(f"{excel_file_path}.xls")
 
 def batch(excel_file_path):
     folder = os.listdir(source_folder)
@@ -312,14 +285,21 @@ def batch(excel_file_path):
             file_names.append(file_name)
             processing(excel_file_path)
             open_vscode()
-            if input("Want to Continue to the Next File? (y/n): ") == 'y':
-                open_next_file()
-                tm.sleep(1)
-                done_with_file(file)
-            else:
+            if i == len(folder):
                 print("Thank you")
-                save_to_excel()
+                save_to_excel(excel_file_path)
                 exit()
+            else:
+                if input("Want to Continue to the Next File? (y/n): ") == 'y':
+                    open_next_file()
+                    tm.sleep(1)
+                    done_with_file(file)
+                else:
+                    print("Thank you")
+                    save_to_excel(excel_file_path)
+                    exit()
+    print("Thank you")
+    save_to_excel(excel_file_path)
 
 def main():
     print("----------------------------------------------------------------")
